825cc3237dafff7814f47deb6e5405f4d65b45a1
Mohammed Ismail <m.ismail.x2@gmail.com>
2025-06-05 21:16:53 +0200

    Fix: import with new name "typer"

--- core/urls.py
+++ core/urls.py
@@ -18,7 +18,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 import debug_toolbar
-from turbo_typing import views
+from typer import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
