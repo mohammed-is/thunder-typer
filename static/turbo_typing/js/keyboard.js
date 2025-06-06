f3527855412799b32acae2837d9e6b2c4c3b2dd5
Mohammed Ismail <m.ismail.x2@gmail.com>
2025-06-06 10:26:54 +0200

    Fix: align keyboard edges

--- static/turbo_typing/js/keyboard.js
+++ static/turbo_typing/js/keyboard.js
@@ -5,10 +5,10 @@ document.addEventListener('DOMContentLoaded', function() {
 
     // Keyboard layout (English/Arabic)
     const enRows = [
-        ['`','1','2','3','4','5','6','7','8','9','0','-','=','Backspace'],
+        ['`','1','2','3','4','5','6','7','8','9','0','-','=',' Backspace '],
         [' Tab ','q','w','e','r','t','y','u','i','o','p','[',']','\\', '     '],
-        [' Caps  ','a','s','d','f','g','h','j','k','l',';','\'','      Enter'],
-        [' Shift   ','z','x','c','v','b','n','m',',','.','/','       Shift'],
+        [' Caps  ','a','s','d','f','g','h','j','k','l',';','\'','     Enter '],
+        [' Shift   ','z','x','c','v','b','n','m',',','.','/','       Shift '],
         [' ctrl ', 'fn', '   ', ' alt ', '         Space            ', ' alt ', '   ', ' ctrl ']
     ];
     const arRows = [
